7254e759b3eacf2dcf68181730e2ebc43a3330a9
duytan2407 <nguyendam554@gmail.com>
2022-03-11 14:01:01 +0700

    Tín : thay đổi login thành -> đăng nhập

--- Nhom_LTTCT/Nhom_LTTCT/Form1.Designer.cs
+++ Nhom_LTTCT/Nhom_LTTCT/Form1.Designer.cs
@@ -76,11 +76,11 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(208, 175);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 4;
-            this.button1.Text = "btnlogin";
+            this.button1.Text = "Đăng Nhập";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
