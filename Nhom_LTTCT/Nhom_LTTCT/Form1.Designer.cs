cc553525fab63beec91705ad9c923b1026c70c19
Ny4b0r0n <nguyenquoccuong290900@gmail.com>
2022-03-11 13:50:30 +0700

    C sua label MatKhau, txtmatkhau, them btnlogin

--- Nhom_LTTCT/Nhom_LTTCT/Form1.Designer.cs
+++ Nhom_LTTCT/Nhom_LTTCT/Form1.Designer.cs
@@ -31,7 +31,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtmatkhau = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -58,26 +59,36 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(76, 98);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Mật khẩu";
+            this.label2.Text = "Password";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // txtmatkhau
+            // txtpassword
             // 
-            this.txtmatkhau.Location = new System.Drawing.Point(156, 98);
-            this.txtmatkhau.Margin = new System.Windows.Forms.Padding(2);
-            this.txtmatkhau.Name = "txtmatkhau";
-            this.txtmatkhau.PasswordChar = '$';
-            this.txtmatkhau.Size = new System.Drawing.Size(128, 20);
-            this.txtmatkhau.TabIndex = 3;
+            this.txtpassword.Location = new System.Drawing.Point(156, 98);
+            this.txtpassword.Margin = new System.Windows.Forms.Padding(2);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '$';
+            this.txtpassword.Size = new System.Drawing.Size(128, 20);
+            this.txtpassword.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(156, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "btnlogin";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Controls.Add(this.txtmatkhau);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtpassword);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.label1);
@@ -94,7 +105,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtUserName;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtmatkhau;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Button button1;
     }
 }
 
